eb30025c881597154837de7a27527ed2c68d0ccb
赵悦岑 <zz2745518585@gmail.com>
2023-11-25 08:38:47 +0800

    fix: update discussions repository (#21)

--- src/discussion.ts
+++ src/discussion.ts
@@ -19,7 +19,7 @@ export function DiscussionCard() {
 
         GM_addElement('script', {
             'src': 'https://giscus.app/client.js',
-            'data-repo': 'Sukwants/Ex124OJ-discussions',
+            'data-repo': 'Ex124OJ/Ex124OJ-discussions',
             'data-repo-id': 'R_kgDOImiZLA',
             'data-category': 'Ideas',
             'data-category-id': 'DIC_kwDOImiZLM4CTCIj',
